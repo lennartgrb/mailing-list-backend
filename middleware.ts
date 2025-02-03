871f903239ce19025eed85264da1a218cf5139f4
lennartgarbe <lennart-garbe@web.de>
2025-02-03 09:40:19 +0100

    renamed data to typescript

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+export function errorMiddleware(err, req, res, next) {
+  if (err.name === "SyntaxError") {
+    console.error(err.message);
+    res.status(500).json({ error: "Invalid JSON!", message: err.message });
+  }
+
+  if (err) {
+    res.status(500).json({ error: "Random Error!", message: err.message });
+  }
+
+  next();
+}
